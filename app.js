0ec64dfdd856f1bbc0edac4840da4a5c453edc53
TAPAKAHOKOT <tapakahokot@gmail.com>
2023-11-04 16:59:01 +0400

    change stars

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-class Rating {
-  constructor(dom) {
-    dom.innerHTML = '<svg width="110" height="20"></svg>';
-    this.svg = dom.querySelector('svg');
-    for(var i = 0; i < 5; i++)
-      this.svg.innerHTML += `<polygon data-value="${i+1}"
-           transform="translate(${i*22},0)" 
-           points="10,1 4,19.8 19,7.8 1,7.8 16,19.8">`;
-    this.svg.onclick = e => this.change(e);
-    this.render();
-  }
-  
-  change(e) {
-    let value = e.target.dataset.value;
-    value && (this.svg.parentNode.dataset.value = value); 
-    starsNum = value;
-    this.render();
-  }
-  
-  render() {
-    this.svg.querySelectorAll('polygon').forEach(star => {
-      let on = +this.svg.parentNode.dataset.value >= +star.dataset.value;
-      star.classList.toggle('active', on);
-    });
-  }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-	document.querySelectorAll('.rating').forEach(dom => new Rating(dom));
+	document.querySelectorAll('.star').forEach(el => {
+		el.addEventListener("click", () => {
+			starsNum = el.value;
+		})
+	});
 });
 
 
