a57478c4553030361430aafaaf7375d8b2b04caf
TAPAKAHOKOT <tapakahokot@gmail.com>
2023-11-04 16:14:45 +0400

    add form

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
+class Rating {
+  constructor(dom) {
+    dom.innerHTML = '<svg width="110" height="20"></svg>';
+    this.svg = dom.querySelector('svg');
+    for(var i = 0; i < 5; i++)
+      this.svg.innerHTML += `<polygon data-value="${i+1}"
+           transform="translate(${i*22},0)" 
+           points="10,1 4,19.8 19,7.8 1,7.8 16,19.8">`;
+    this.svg.onclick = e => this.change(e);
+    this.render();
+  }
+  
+  change(e) {
+    let value = e.target.dataset.value;
+    value && (this.svg.parentNode.dataset.value = value); 
+    starsNum = value;
+    this.render();
+  }
+  
+  render() {
+    this.svg.querySelectorAll('polygon').forEach(star => {
+      let on = +this.svg.parentNode.dataset.value >= +star.dataset.value;
+      star.classList.toggle('active', on);
+    });
+  }
+}
+document.querySelectorAll('.rating').forEach(dom => new Rating(dom));
+
+
+
 var tg = window.Telegram.WebApp,
-	mainButton = tg.MainButton;
+	mainButton = tg.MainButton,
+	starsNum = 0;
 
 mainButton.show();
 mainButton.enable();
@@ -9,9 +40,9 @@ mainButton.color = '#2cab37';
 mainButton.setText("Отправить");
 
 tg.onEvent("mainButtonClicked", function(){
-	tg.sendData('TEST')
 	tg.sendData(JSON.stringify({
-		'stars': 5,
-		'comment': document.getElementById('comment').value
+		'stars': starsNum,
+		'comment': document.getElementById('comment').value,
+		'is_anano': document.getElementById('is_anon').checked,
 	}));
 });
