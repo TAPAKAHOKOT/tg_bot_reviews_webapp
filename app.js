0d267295ba179735130f7e22e4c9ad15fa655bb1
TAPAKAHOKOT <tapakahokot@gmail.com>
2023-11-04 14:41:52 +0400

    add base data

---
+++ app.js
@@ -0,0 +1,15 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
+
+tg.MainButton.setText("Отправить");
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData({
+		'stars': 5,
+		'comment': document.getElementById('comment').text
+	});
+});
