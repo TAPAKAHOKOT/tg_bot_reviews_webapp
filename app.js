3d8f06bd0b67b81db685b54204efc058421fc36b
TAPAKAHOKOT <tapakahokot@gmail.com>
2023-11-04 15:34:01 +0400

    fixes

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ mainButton.textColor = '#FFFFFF';
 mainButton.color = '#2cab37';
 mainButton.setText("Отправить");
 
-tg.onEvent("mainButtonClicked", function(){
+tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
+
+mainButton.onEvent("click", function(){
 	tg.showAlert(`Добро пожаловать, @${tg.WebAppUser.username}.`);
 	tg.sendData({
 		'stars': 5,
